282fc62d823947f62a01fed292eeab9f32e70fec
Simon <cerberus.anonymus@gmail.com>
2022-04-02 21:49:23 +0200

    implemented Question-class

--- src/Question.java
+++ src/Question.java
@@ -4,10 +4,32 @@ public class Question {
 
     private char correctOption;
 
+    private boolean isDrawn;
+    //  isDrawn is used to flag "Question"-objects as 'already played' so that players don't get questions twice in one game.
 
-    Question (){
 
-        
+    public Question(String[] questionElements){
+
+        question = questionElements[0];
+        optionA = questionElements[1];
+        optionB = questionElements[2];
+        optionC = questionElements[3];
+        optionD = questionElements[4];
+        correctOption = questionElements[5].charAt(0);
+
     }
+    //  The "Question"-constructor uses an "questionElements"-array that holds all attributes.
     
+    public void print(){
+        System.out.println(question + "\nA: " + optionA + "\nB: " + optionB + "\nC: " + optionC + "\nD: " + optionD);
+    }
+
+    public boolean isDrawn(){
+        return isDrawn;        
+    }
+
+    public void setDrawn(){
+        isDrawn = true;
+    }   
+
 }
