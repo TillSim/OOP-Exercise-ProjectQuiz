413ddcf913c169af89632154f8b5f4c25d2f202d
Simon <cerberus.anonymus@gmail.com>
2022-04-02 14:18:44 +0200

    drafted first approach with ArrayList

---
+++ src/Question.java
@@ -0,0 +1,13 @@
+public class Question {
+
+    private String question, optionA, optionB, optionC, optionD;
+
+    private char correctOption;
+
+
+    Question (){
+
+        
+    }
+    
+}
---
+++ src/QuestionPool.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class QuestionPool {
+    
+    public ArrayList<Question> questions = new ArrayList<>();
+
+    
+
+
+
+
+
+
+}
