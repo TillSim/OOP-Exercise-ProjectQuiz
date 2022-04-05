1918ff21d676361897425675395b5fd981c15032
Simon <cerberus.anonymus@gmail.com>
2022-04-05 16:18:26 +0200

    implemented first test case

--- src/App.java
+++ src/App.java
@@ -1,4 +1,18 @@
-public class App {
+import java.io.File;
+import java.util.Scanner;
+
+public abstract class App {
     public static void main(String[] args) throws Exception {  
+
+        Scanner consoleInput = new Scanner(System.in);
+
+        File questionSetFile = new File("testQuestionSet.csv");
+
+        Quiz quiz = new Quiz(consoleInput,questionSetFile);
+
+        quiz.run(consoleInput);
+
+        consoleInput.close();
+
     }
 }
